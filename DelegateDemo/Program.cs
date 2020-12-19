77d85e53ae8d9b8f956751335cacc03caaa21206
Edward Ziemiński <edward.zieminski@gmail.com>
2020-12-19 20:46:41 +0100

    With Actions instead of actual methods

--- DelegateDemo/Program.cs
+++ DelegateDemo/Program.cs
@@ -1,19 +1,31 @@
 ﻿using System;
 using System.Diagnostics;
 
-Machine machine = new Machine();
+Action toDo1 = () =>
+{
+    Console.WriteLine("Machine 1 is starting.");
+    Console.WriteLine("Machine 1 is working now...");
+    Console.WriteLine("Machine 1 is closing.");
+};
 
-var machine1Metrics = machine.Run(Machine.ToDo1);
+Action toDo2 = () =>
+{
+    Console.WriteLine("Machine 2 is starting.");
+    Console.WriteLine("Machine 2 is working now...");
+    Console.WriteLine("Machine 2 is closing.");
+};
+
+var machine1Metrics = Machine.Run(toDo1);
 Console.WriteLine($"Machine 1 was running for { machine1Metrics.StopWatch.ElapsedMilliseconds } ms");
 
-var machine2Metrics = machine.Run(MachineExternalMethods.ToDo2);
+var machine2Metrics = Machine.Run(toDo2);
 Console.WriteLine($"Machine 2 was running for { machine2Metrics.StopWatch.ElapsedMilliseconds } ms");
 
-public class Machine
+public static class Machine
 {
     public delegate void ToDo();
 
-    public MachineResults Run(ToDo toDoMethod)
+    public static MachineResults Run(Action toDoMethod)
     {
         var stopWatch = new Stopwatch();
 
@@ -23,23 +35,6 @@ public class Machine
 
         return new ( toDoMethod, stopWatch );
     }
-
-    public static void ToDo1()
-    {
-        Console.WriteLine("Odpalam maszynę 1");
-        Console.WriteLine("Wrum wrum 1");
-        Console.WriteLine("Zamykam maszynę 1");
-    }
 }
 
-public class MachineExternalMethods
-{
-    public static void ToDo2()
-    {
-        Console.WriteLine("Odpalam maszynę 2");
-        Console.WriteLine("Wrum wrum 2");
-        Console.WriteLine("Zamykam maszynę 2");
-    }
-}
-
-public record MachineResults(Machine.ToDo ToDoMethod, Stopwatch StopWatch);
+public record MachineResults(Action ToDoMethod, Stopwatch StopWatch);
